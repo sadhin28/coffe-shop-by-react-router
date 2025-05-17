f555ba97a98e2411e4b23fb2b735a8275484fa1c
sadhin28 <taosifsadhin113@gmail.com>
2025-05-18 02:22:52 +0600

    add category

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,29 +1,22 @@
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
+import { Link, useLoaderData } from "react-router-dom";
 import Heading from "../Heading/Heading";
 import Hero from "../Hero/Hero";
 const Home = () => {
+    const categorie = useLoaderData();
+    
     return (
         <div>
             <Hero></Hero>
             <Heading title={'Browse Coffees By Category'} subTitle={'Choose Your Coffee'} ></Heading>
             <div>
-              
-                <Tabs>
-                    <TabList>
-                        <Tab>Browse Coffee</Tab>
-                        <Tab>Title 2</Tab>
-                        <Tab>Title 3</Tab>
-                    </TabList>
 
-                    <TabPanel>
-                        <h2>Any content 1</h2>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>Any content 2</h2>
-                    </TabPanel>
-                </Tabs>
-              
+                <div role="tablist" className="tabs tabs-lift justify-between ">
+                    {
+                        categorie.map(data=><P data={data.category}></P>)
+                    }
+                  
+                </div>
             </div>
         </div>
     );
