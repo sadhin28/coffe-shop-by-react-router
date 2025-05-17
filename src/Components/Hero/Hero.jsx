e755e8e47a1ebf01c938583c59e638d32fb5b785
sadhin28 <taosifsadhin113@gmail.com>
2025-05-17 21:48:08 +0600

    add header img

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import Banner from '../../assets/banner.jpg'
 const Hero = () => {
     return (
         <div className='mt-10 mb-10'>
-           <img className='' src={Banner} alt="" />
+           <img  className='w-150 mx-auto h-80 md:h-150 md:w-400' src={Banner} alt="" />
         </div>
     );
 };
