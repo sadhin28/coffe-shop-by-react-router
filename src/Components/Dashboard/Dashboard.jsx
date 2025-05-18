7a2980a9a50d6110dd25f6e1386576baccad4470
sadhin28 <taosifsadhin113@gmail.com>
2025-05-19 01:13:46 +0600

    add storecoffee

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,16 +1,26 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Heading from '../Heading/Heading';
 import { useLoaderData } from 'react-router-dom';
+import { getStoredReadList } from '../Utility/addlocal';
+import Card from '../Card/Card';
 
 const Dashboard = () => {
     const [addCoffee,setaddCoffee]=useState([]);
     const allCoffee=useLoaderData();
+    console.log(addCoffee)
+    useEffect(()=>{
+        const storeCoffee=getStoredReadList();
+        const storeCoffeList 
+    },[])
     return (
         <div>
            <div className='mt-10'> <Heading
              title={"Welcome to Dashboard"}
              subTitle={"Manage Coffe that that you have prviously added as favorive,You can view or remove them from here"}></Heading>
+            </div>
+            <div>
+                
             </div>
         </div>
     );
