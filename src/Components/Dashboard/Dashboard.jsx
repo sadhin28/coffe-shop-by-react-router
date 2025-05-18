2f9b373733121dfe424aec2061975a314fb3c8fd
sadhin28 <taosifsadhin113@gmail.com>
2025-05-19 01:46:15 +0600

    add dashboard coffee

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,26 +1,32 @@
-
-import { useEffect, useState } from 'react';
-import Heading from '../Heading/Heading';
 import { useLoaderData } from 'react-router-dom';
+import Heading from '../Heading/Heading';
+import { useEffect, useState } from 'react';
 import { getStoredReadList } from '../Utility/addlocal';
 import Card from '../Card/Card';
 
+
 const Dashboard = () => {
-    const [addCoffee,setaddCoffee]=useState([]);
-    const allCoffee=useLoaderData();
-    console.log(addCoffee)
-    useEffect(()=>{
-        const storeCoffee=getStoredReadList();
-        const storeCoffeList 
-    },[])
+   const allcoffee = useLoaderData();
+   const [addcoffee,setaddcoffee]=useState([])
+   console.log(addcoffee)
+   console.log(allcoffee)
+   useEffect(()=>{
+     const storedCoffee = getStoredReadList();
+     const storedCoffeeint = storedCoffee.map(id=>parseInt(id));
+     const coffeelist = allcoffee.filter(c=>storedCoffeeint.includes(c.id))
+     setaddcoffee(coffeelist)
+   },[])
+
     return (
         <div>
            <div className='mt-10'> <Heading
              title={"Welcome to Dashboard"}
              subTitle={"Manage Coffe that that you have prviously added as favorive,You can view or remove them from here"}></Heading>
             </div>
-            <div>
-                
+            <div className='grid justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {
+                  addcoffee.map(data=><Card data={data}></Card>)
+                }
             </div>
         </div>
     );
