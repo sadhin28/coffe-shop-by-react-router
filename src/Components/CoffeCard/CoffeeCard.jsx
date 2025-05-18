2712dd72ee145d88a94b90fdf423989a7ec712b5
sadhin28 <taosifsadhin113@gmail.com>
2025-05-18 15:42:53 +0600

    add coffee card

--- src/Components/CoffeCard/CoffeeCard.jsx
+++ src/Components/CoffeCard/CoffeeCard.jsx
@@ -12,10 +12,12 @@ const CoffeeCard = () => {
     console.log(coffee)
     return (
         <div>
-            <h1 className="font-bold mb-10 text-2xl">{obj.category}</h1>
-            {
+            <h1 className="font-bold  text-center  mb-10 text-2xl">{obj.category}</h1>
+           <div className="grid mb-10 gap-10 justify-items-center md:grid-cols-2 lg:grid-cols-3">
+               {
               coffee.map(data=><Card data={data}></Card>)
-            }
+              }
+           </div>
         </div>
     );
 };
