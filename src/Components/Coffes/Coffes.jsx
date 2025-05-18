2cad540e81c494286e3b4d43519db3be0d586e2a
sadhin28 <taosifsadhin113@gmail.com>
2025-05-18 19:45:13 +0600

    add card

--- src/Components/Coffes/Coffes.jsx
+++ src/Components/Coffes/Coffes.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Card from '../Card/Card';
 
 const Coffes = () => {
+    const data =useLoaderData()
     return (
-        <div>
-            <h1>Coffes</h1>
+        <div className='mt-10 mb-10 grid  justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-10'>
+          {
+            data.map(data=><Card data={data}></Card>)
+          }
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -42,7 +42,8 @@ const router = createBrowserRouter([
       },
       {
         path:"coffes",
-        element:<Coffes></Coffes>
+        element:<Coffes></Coffes>,
+        loader:()=>fetch('../coffees.json')
       },
       {
         path:"dashboard",
