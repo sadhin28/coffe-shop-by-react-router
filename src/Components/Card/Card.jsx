2f6007c3e0c39309cff3a6eb32645ac6295b5957
sadhin28 <taosifsadhin113@gmail.com>
2025-05-18 16:56:19 +0600

    add data in local store

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const Card = ({ data}) => {
     const {image,name,id,ingredients,description}=data
     const handelAddToCard=(id)=>{
-        alert(id)
+        console.log(typeof(id))
+        
     }
     return (
         <div>
