03f6482671d32f4bc1c24c62b787250a2581be0f
sadhin28 <taosifsadhin113@gmail.com>
2025-05-19 17:48:08 +0600

    responsive footer section

--- src/Components/Footer/Footer2.jsx
+++ src/Components/Footer/Footer2.jsx
@@ -5,38 +5,44 @@ import { FaGithub } from "react-icons/fa";
 import logo from '../../assets/WhatsApp Image 2025-05-19 at 04.07.44_a63162ba.jpg'
 const Footer2 = () => {
     const product = <>
-        <li><a href="">Ullo Mart Mail</a></li>
+       <div className="">
+          <li><a href="">Ullo Mart Mail</a></li>
         <li><a href="">Ullo Mart Calender</a></li>
+       </div>
 
     </>
     const Company = <>
-        <li><a href="">About</a></li>
+       <div>
+         <li><a href="">About</a></li>
         <li><a href="">Team</a></li>
         <li><a href="">Shop</a></li>
         <li><a href="">Contact Us</a></li>
+       </div>
 
 
     </>
     const Contact = <>
-        <li><a href="">Blog</a></li>
+       <div>
+         <li><a href="">Blog</a></li>
         <li><a href="">Help and Support</a></li>
         <li><a href="">Press and Media</a></li>
-        <li><a href="">Contact Us</a></li>
+        <li><a href="">About Us</a></li>
+       </div>
 
 
     </>
     return (
-        <footer className="p-5 border-t-1 bg-black">
+        <footer className="p-5  bg-purple-50  shadow-sm">
             <div className="container grid gap-6 py-6 md:gap-12 md:py-12">
                 <div className="flex justify-items-center">
-                    <div className='text-purple-800 gap-40 w-3/10'>
+                    <div className=' gap-40 w-3/10'>
 
                         
                        <div className="mb-5 md:flex md:justify-center ">
                           <img className="rounded-full flex md:h-20 md:w-20 relative left-30 md:left-0" src={logo} alt="" />
                         
                        </div>
-                        <ul className="text-4xl flex  gap-6 md:justify-center ">
+                        <ul className="text-4xl flex transform-3d gap-6 md:justify-center ">
                             <li><a target="-blank" href="https://www.facebook.com/profile.php?id=61575997992841"><FaFacebookSquare /></a></li>
                             <li><a target="-blank" href=""><IoLogoInstagram /></a></li>
                             <li><a target="-blank" href=""><FaGithub /></a></li>
@@ -47,31 +53,31 @@ const Footer2 = () => {
                     </div>
 
                     <div className="grid grid-cols-3 justify-items-center w-7/10">
-                        <div className="text-purple-800 mt-5 invisible h-0 md:h-full md:visible">
-                            <h1 className="font-black text-2xl">Company</h1>
+                        <div className=" mt-5 invisible h-0 md:h-full md:visible">
+                            <h1 className=" text-purple-600 font-bold text-2xl">Company</h1>
                             <ul className="grid text-2xl">
                                 {Company}
                             </ul>
                         </div>
 
-                        <div className="text-purple-800 mt-5 md:h-full h-0 invisible md:visible ">
-                            <h1 className="font-black text-2xl">Product</h1>
+                        <div className=" mt-5 md:h-full h-0 invisible md:visible ">
+                            <h1 className=" text-2xl text-purple-600 font-bold">Product</h1>
                             <ul className="grid text-2xl">
                                 {product}
                             </ul>
                         </div>
-                        <div className="text-purple-800 mt-5 md:h-full h-0 invisible md:visible ">
-                            <h1 className="font-black text-2xl">Contact</h1>
+                        <div className=" mt-5 md:h-full h-0 invisible md:visible ">
+                            <h1 className=" text-2xl text-purple-600 font-bold">Contact</h1>
                             <ul className="grid text-2xl">
                                 {Contact}
                             </ul>
                         </div>
                     </div>
                 </div>
-                <div className="text-purple-800 grid gap-5 mt-5 ">
+                <div className=" grid gap-5 mt-5 ">
                     <div className="collapse collapse-plus text-2xl md:hidden ">
                         <input type="radio" name="my-accordion-3" defaultChecked />
-                        <div className="collapse-title font-semibold">Product</div>
+                        <div className="collapse-title text-purple-600  font-semibold">Product</div>
                         <div className="collapse-content text-sm collapse-minus">
                             <ul className="grid gap-5 text-2xl">
                                 {product}
@@ -81,7 +87,7 @@ const Footer2 = () => {
                     </div>
                     <div className="collapse collapse-plus md:hidden text-2xl ">
                         <input type="radio" name="my-accordion-3" />
-                        <div className="collapse-title font-semibold">Company</div>
+                        <div className="collapse-title text-purple-600  font-semibold">Company</div>
                         <div className="collapse-content text-sm">
                             <ul className="grid gap-5 text-2xl">
                                 {Company}
@@ -90,7 +96,7 @@ const Footer2 = () => {
                     </div>
                     <div className="collapse collapse-plus md:hidden text-2xl ">
                         <input type="radio" name="my-accordion-3"  />
-                        <div className="collapse-title font-semibold">Contact</div>
+                        <div className="collapse-title text-purple-600  font-semibold">Contact</div>
                         <div className="collapse-content text-sm">
                             <ul className="grid gap-5 text-2xl">
                                 {Contact}
@@ -100,10 +106,10 @@ const Footer2 = () => {
 
 
                 </div>
-                <div className="divider"></div>
-                <div className="text-center">
+                <div className="flex-1 border-b  p-2"></div>
+                <div className="text-center ">
                     <aside>
-                        <p>Copyright © {new Date().getFullYear()} - All right reserved by Ullo Mart</p>
+                       <small> <p>Copyright © {new Date().getFullYear()} - All right reserved by Ullo Mart</p></small>
                     </aside>
                 </div>
             </div>
