5246d2fd88916a8212c4fe37a1bbf465c4bc2831
sadhin28 <taosifsadhin113@gmail.com>
2025-05-20 23:33:16 +0600

    add faq data from json

--- src/Components/DataFaq.jsx
+++ src/Components/DataFaq.jsx
@@ -1,12 +1,12 @@
 
 const DataFaq = ({ faq }) => {
-    const {question,}=faq
+    const {question,answer}=faq
     return (
         <div>
             <details className="collapse collapse-arrow bg-base-100 border-base-300 border">
-                <summary className="collapse-title font-semibold">How do I create an account?</summary>
+                <summary className="collapse-title font-semibold">{question}</summary>
                 <div className="collapse-content text-sm">
-                    Click the "Sign Up" button in the top right corner and follow the registration process.
+                   {answer}
                 </div>
             </details>
         </div>
